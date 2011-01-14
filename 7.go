bccbed80c73443f69a8ddbe023f508de96d35607
Chidambaram Annamalai <quantumelixir@gmail.com>
2011-01-14 21:56:46 +0530

    Faster prime checking
    Primes larger than 3 are of the form 6k+1/6k-1

--- 7.go
+++ 7.go
@@ -7,17 +7,21 @@ func main() {
     var prime int
     nth := 10001
 
-    if nth == 1 {
-        prime = 2
+    switch nth {
+        case 1: prime = 2
+        case 2: prime = 3
     }
 
     sqrt := 1
-    I: for p, i := 1, 3; p < nth; i+=2 {
+    I: for p, i := 2, 5; p < nth; i+=2 {
         for sqrt*sqrt < i {
             sqrt++
         }
-        for j := 2; j < sqrt + 1; j++ {
-            if i % j == 0 {
+        if i % 3 == 0 {
+            continue I
+        }
+        for j := 5; j <= sqrt; j+=6 {
+            if i % j == 0 || i % (j + 2) == 0 {
                 continue I
             }
         }
