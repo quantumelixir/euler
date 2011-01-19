72735a4a896005f1f91ed327bbeed54801b67be2
Chidambaram Annamalai <quantumelixir@gmail.com>
2011-01-19 22:11:16 +0530

    37

---
+++ 37.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+    "fmt"
+    "utils"
+)
+
+func isRightTruncatablePrime(n int) (bool) {
+    for n != 0 {
+        if !utils.IsPrime(n) {
+            return false
+        }
+        n /= 10
+    }
+    return true
+}
+
+func isLeftTruncatablePrime(n int) (bool) {
+    n = utils.ReverseInt(n)
+    for n != 0 {
+        if !utils.IsPrime(utils.ReverseInt(n)) {
+            return false
+        }
+        n /= 10
+    }
+    return true
+}
+
+func isTruncatablePrime(n int) (bool) {
+    return isRightTruncatablePrime(n) && isLeftTruncatablePrime(n)
+}
+
+func main() {
+
+    sum := 0
+    for count, i := 0, 10; count < 11; i++ {
+        if isTruncatablePrime(i) {
+            sum += i
+            count++
+        }
+    }
+
+    fmt.Println(sum)
+}
