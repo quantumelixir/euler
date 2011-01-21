bce91b47c681459b446540db5ba9b3ab5cdcdc65
Chidambaram Annamalai <quantumelixir@gmail.com>
2011-01-22 00:52:38 +0530

    utils

--- utils/utils.go
+++ utils/utils.go
@@ -1,5 +1,22 @@
 package utils
 
+func Abs(a int) (int) {
+    if a > 0 { return a }
+    return -a
+}
+
+func AbsSum(a []int) (int) {
+    sum := 0
+    for _, i := range a { sum += Abs(i) }
+    return sum
+}
+
+func Sum(a []int) (int) {
+    sum := 0
+    for _, i := range a { sum += i }
+    return sum
+}
+
 func Min(a, b int) (int) {
     if a < b {
         return a
@@ -14,30 +31,6 @@ func Max(a, b int) (int) {
     }
 
     return b
-}
-
-func IsPrime(n int) (bool) {
-
-    if n == 2 || n == 3 {
-        return true
-    }
-
-    if n == 1 || n % 2 == 0 || n % 3 == 0 {
-        return false
-    }
-
-    sqrt := 1
-    for sqrt*sqrt < n {
-        sqrt++
-    }
-
-    for j := 5; j <= sqrt; j += 6 {
-        if n%j == 0 || n%(j+2) == 0 {
-            return false
-        }
-    }
-
-    return true
 }
 
 func RotateInt(n int) (int) {
@@ -66,6 +59,30 @@ func ReverseInt(n int) (int) {
     return res
 }
 
+func IsPrime(n int) (bool) {
+
+    if n == 2 || n == 3 {
+        return true
+    }
+
+    if n == 1 || n % 2 == 0 || n % 3 == 0 {
+        return false
+    }
+
+    sqrt := 1
+    for sqrt*sqrt < n {
+        sqrt++
+    }
+
+    for j := 5; j <= sqrt; j += 6 {
+        if n%j == 0 || n%(j+2) == 0 {
+            return false
+        }
+    }
+
+    return true
+}
+
 //
 // Return a boolean slice with false in positions whose indices are primes,
 // upto (but not including) n.
@@ -81,3 +98,17 @@ func Sieve(n int) (sieve []bool) {
     return sieve
 }
 
+func Primes(n int) (primes []int) {
+
+    count, sieve := 0, Sieve(n)
+    for _, i := range sieve { if !i { count++ } }
+
+    k, primes := 0, make([]int, count)
+    for i, j := range sieve {
+        if !j {
+            primes[k] = i
+            k++
+        }
+    }
+    return primes
+}
