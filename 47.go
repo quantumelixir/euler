7acfffc3cb1958c494b26c0fcab1b98d4cb934e5
Chidambaram Annamalai <quantumelixir@gmail.com>
2011-01-24 15:26:39 +0530

    47

---
+++ 47.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+    "fmt"
+)
+
+func countDistinctFactors(limit int) []int {
+    count := make([]int, limit)
+    count[0], count[1] = -1, 1
+    for i := 2; i < limit; i++ {
+        if count[i] == 0 {
+            for j := 2*i; j < limit; j += i {
+                count[j]++
+            }
+        }
+    }
+    return count
+}
+
+func main() {
+    const limit, k = 1E6, 4
+    count := countDistinctFactors(limit)
+
+    for i := 0; i + k - 1 < limit ; i++ {
+        flag := true
+        for j := 0; flag && j < k; j++ {
+            flag = flag && count[i + j] == k
+        }
+        if flag {
+            fmt.Println(i)
+            break
+        }
+    }
+}
