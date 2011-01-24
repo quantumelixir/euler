90b271634823d6301f9f172f4300a8c885d54012
Chidambaram Annamalai <quantumelixir@gmail.com>
2011-01-24 21:35:37 +0530

    34

---
+++ 34.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func factorial(n int) int {
+    c := 1
+    for i := 1; i <= n; i++ { c *= i }
+    return c
+}
+
+func fsum(n int) int {
+    r := 0
+    for n != 0 {
+        t := n%10
+        r += factorial(t)
+        n /= 10
+    }
+    return r
+}
+
+func main() {
+	sum := 0
+	for i := 3; i < 1E6; i++ {
+		if fsum(i) == i { sum += i }
+	}
+	fmt.Println(sum)
+}
