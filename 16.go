ae5bd320e973bdc0170bd0b33e1e5829ba42be0a
Chidambaram Annamalai <quantumelixir@gmail.com>
2011-01-25 00:44:08 +0530

    16

---
+++ 16.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+    "fmt"
+    "utils"
+)
+
+type BigInt []int // base 10
+
+func lmultiply(a BigInt, b BigInt) BigInt {
+    carry, orig := 0, len(a)
+    for i := 0; ; i++ {
+        if i == len(a) {
+            a = a[:len(a) + 1]
+        }
+        a[i] = a[i]*b[0]
+        for j := 0; j < i; j++ {
+            if j < len(a) && i - j < len(b) { a[i] += a[j]*b[i-j] }
+        }
+        if i >= orig-1 && a[i] + carry == 0 { break }
+        carry += a[i]
+        a[i] = carry%10
+        carry /= 10
+    }
+
+    return a
+}
+
+func main() {
+    res := make([]int, 1, 1000)
+    base := make([]int, 1)
+    res[0] = 2
+    base[0] = 2
+
+    for i := 2; i <= 1000; i++ {
+        res = lmultiply(res, base)
+    }
+    fmt.Println(utils.Sum(res))
+}
