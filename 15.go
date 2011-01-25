6bd5e0bd6c88d8e0f3116c55349fb01d03c09b1c
Chidambaram Annamalai <quantumelixir@gmail.com>
2011-01-26 03:40:06 +0530

    15

---
+++ 15.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+    "fmt"
+)
+
+func main() {
+    const n = 21
+    ways := make([][]uint64, n)
+    for i := 0; i < n; i++ {
+        ways[i] = make([]uint64, n)
+        ways[i][0], ways[0][i] = 1, 1
+    }
+
+    for i := 1; i < n; i++ {
+        for j := 1; j < n; j++ {
+            ways[i][j] = ways[i][j-1] + ways[i-1][j]
+        }
+    }
+
+    fmt.Println(ways[n-1][n-1])
+}
