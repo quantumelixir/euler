e17221a12390a585e2f663a37c2f5c0827aac224
Chidambaram Annamalai <quantumelixir@gmail.com>
2011-01-26 19:16:20 +0530

    Simplify the function for finding the digit

--- 40.go
+++ 40.go
@@ -5,37 +5,24 @@ import (
     "utils"
 )
 
-func getDigit(n, p int) (digit int) {
-    if n == 0 { return 0 }
-
-    t, tot := n, 0
-    for t != 0 {
-        t/=10
-        tot++
-    }
-
-    p = tot - p + 1
-    for p != 0 {
-        digit = n%10
-        n/=10
-        p--
-    }
-
-    return digit
-}
-
 func d(n int) int {
-    tot, i, ti := 0, 1, 1
+    tot, i, ti, digit := 0, 1, 1, 0
     for {
         if n <= tot + 9*i*ti {
             rem := n - tot - 1
-            return getDigit(rem/i + ti, rem%i + 1)
+            d, p := rem/i + ti, i - rem%i
+            for p != 0 {
+                digit = d%10
+                d/=10
+                p--
+            }
+            break
         }
         tot += 9*i*ti
         i++
         ti *= 10
     }
-    return i
+    return digit
 }
 
 func main() {
